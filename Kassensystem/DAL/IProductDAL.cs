a73d6c349529d5a514c1e27495e73e2a6b595ad6
Dustin Dyckmanns <dustin.dyckmanns@cgi.com>
2023-08-28 10:24:20 +0200

    Code aufgeräumt

--- Kassensystem/DAL/IProductDAL.cs
+++ Kassensystem/DAL/IProductDAL.cs
@@ -6,15 +6,4 @@ namespace Kassensystem.DAL
     {
         List<Product> SelectAllProducts();
     }
-    public class ProductDAL : IProductDAL
-    {
-        public List<Product> SelectAllProducts() 
-        {
-            List<Product> ListProducts = new List<Product>
-            {
-                new Product {ProductId = 1, Name = "Sandwich-Toast Vollkorn", Description = "Weizenvollkorntoast mit 58% Weizenvollkornmehl", Price = 0.89},
-            };
-            return ListProducts;
-        }
-    }
 }
